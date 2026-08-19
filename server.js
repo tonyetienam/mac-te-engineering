ad730bc7f60446c9677537b2cd471260a2cc723f
Anthony Etienam <tonyetienam@gmail.com>
2026-08-19 15:54:51 +0100

    Added homepage route

--- server.js
+++ server.js
@@ -10,6 +10,12 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
+const path = require('path');
+
+// Serve the static HTML file when visiting the root URL
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 // Database connection
 const pool = new Pool({
